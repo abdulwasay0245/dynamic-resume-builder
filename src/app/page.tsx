a3edf9803a008fa7e00ed1868a6c23c441fd04cd
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-22 12:05:16 +0500

    added skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 'use client'
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 
 export default function MyForm() {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [profession, setProfession] = useState('');
-  const [skills, setSkills] = useState([]);
+  const [singleSkill, setSingleSkill] = useState('')
+  const [skills, setSkills] = useState<string[]>([]);
+
+
 
   const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({ email, name });
     setName('')
     setEmail('')
+    const newSkills = singleSkill.split(',').map(skill => skill.trim())
+    setSkills(newSkills);
+    setSingleSkill('')
+
   };
 
   return (
@@ -47,10 +54,15 @@ export default function MyForm() {
             <input
         type="text"
         placeholder="Enter your Phone Number"
-        value={skills}
-        onChange={(e) => setSkills(e.target.value)}
+        value={singleSkill}
+        onChange={(e) => setSingleSkill(e.target.value)}
           className='border-2 border-black' />
-        {console.log(setSkills)}
+        {skills.map((skill) => (
+          <div key={Math.random()}>
+            <h1>{skill }</h1>
+          </div>
+        ))}
+        
       <button type="submit" className='bg-amber-950 text-white px-5 py-2 rounded-2xl'>Submit</button>
     </form>
         </div>
